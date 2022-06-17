8294a70b84126667fc1b455f4c97992787ba90b9
251378436 <251378436@qq.com>
2022-06-18 00:43:37 +1200

    Update unit test

--- API1.Test/UnitTest1.cs
+++ API1.Test/UnitTest1.cs
@@ -7,7 +7,9 @@ namespace API1.Test
         [Fact]
         public void Test1()
         {
-
+            var a = 123;
+            var b = 123;
+            Assert.Equal(a, b);
         }
     }
 }
