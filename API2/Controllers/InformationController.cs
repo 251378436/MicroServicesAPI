c47cc4c2da299f5ff4c9e7da9cfb2636282b85b2
251378436 <251378436@qq.com>
2022-11-16 23:50:49 +1300

    update information controller

---
+++ API2/Controllers/InformationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            return Ok("1.0.0");
+        }
+    }
+}
